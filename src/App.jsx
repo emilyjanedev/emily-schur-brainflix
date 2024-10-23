a69c8b8406a13277d289d000db08a8d2982c7d57
Emily Schur <ejschur@gmail.com>
2024-10-22 21:20:47 -0700

    Build VideoBank and VideoCard components

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import "./App.scss";
 import Header from "./components/Header/Header";
 import CommentSection from "./components/CommentSection/CommentSection";
 import VideoSection from "./components/VideoSection/VideoSection";
+import VideoBank from "./components/VideoBank/VideoBank";
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
       <Header />
       <VideoSection />
       <CommentSection />
+      <VideoBank />
     </>
   );
 }
