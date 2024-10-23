85fd02f51ce1a3e529b626f1b7a2fb95a867d4f2
Emily Schur <ejschur@gmail.com>
2024-10-22 21:47:07 -0700

    Implement base logic for video bank filtering and rendering

--- src/components/CurrentVideo/CurrentVideo.jsx
+++ src/components/CurrentVideo/CurrentVideo.jsx
@@ -2,10 +2,10 @@ import "./CurrentVideo.scss";
 import PropTypes from "prop-types";
 
 function CurrentVideo({ video }) {
-  const { video: src, image } = video;
+  const { video: src, image, id } = video;
 
   return (
-    <video controls src={src} poster={image} className="video">
+    <video controls src={src} poster={image} className="video" id={id}>
       <p>Your browser does not support HTML video.</p>
     </video>
   );
