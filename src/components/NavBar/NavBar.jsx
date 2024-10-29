07cbcae41d33a6aa398bca95bed43588116bb312
Emily Schur <ejschur@gmail.com>
2024-10-28 17:23:56 -0700

    Link Branflix logo to home page

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -1,11 +1,18 @@
 import "./NavBar.scss";
 import brainflixLogo from "../../assets/images/logo/BrainFlix-logo.svg";
 import SearchForm from "../SearchForm/SearchForm";
+import { Link } from "react-router-dom";
 
 function NavBar() {
   return (
     <nav className="nav-bar">
-      <img src={brainflixLogo} alt="brainflix logo" className="nav-bar__logo" />
+      <Link to="/">
+        <img
+          src={brainflixLogo}
+          alt="brainflix logo"
+          className="nav-bar__logo"
+        />
+      </Link>
       <SearchForm />
     </nav>
   );
