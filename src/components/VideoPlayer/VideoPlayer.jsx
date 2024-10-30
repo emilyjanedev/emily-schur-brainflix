a6c2823ea8182647c5626d2448f7c07e0317dca7
Emily Schur <ejschur@gmail.com>
2024-10-30 08:48:23 -0700

    Add key to make video play

--- src/components/VideoPlayer/VideoPlayer.jsx
+++ src/components/VideoPlayer/VideoPlayer.jsx
@@ -1,15 +1,23 @@
 import "./VideoPlayer.scss";
 import PropTypes from "prop-types";
+import { apiKey } from "../../utils/api";
 
 function VideoPlayer({ activeVideo }) {
   const { video: src, image } = activeVideo;
 
   return (
-    <video controls src={src} poster={image} className="current-video">
+    <video
+      controls
+      key={src}
+      src={`${src}?api_key=${apiKey}`}
+      poster={image}
+      className="current-video"
+    >
       <p>Your browser does not support HTML video.</p>
     </video>
   );
 }
+
 VideoPlayer.propTypes = {
   activeVideo: PropTypes.object.isRequired,
 };
