c4746a1f331dee4c7b7517010970f785f4d83434
Emily Schur <ejschur@gmail.com>
2024-10-29 18:27:50 -0700

    Add tablet/desktop styling to VideoUploadForm

--- src/components/VideoUploadForm/VideoUploadForm.jsx
+++ src/components/VideoUploadForm/VideoUploadForm.jsx
@@ -6,37 +6,41 @@ import videoThumbnail from "../../assets/images/Upload-video-preview.jpg";
 function VideoUploadForm() {
   return (
     <form className="video-upload-form">
-      <label htmlFor="thumbnail" className="video-upload-form__label">
-        VIDEO THUMBNAIL
-      </label>
-      <img
-        src={videoThumbnail}
-        alt=""
-        className="video-upload-form__thumbnail"
-      />
-      <label htmlFor="videoTitle" className="video-upload-form__label">
-        TITLE YOUR VIDEO
-      </label>
-      <input
-        type="text"
-        name="videoTitle"
-        id="videoTitle"
-        className="video-upload-form__input"
-        placeholder="Add a title to your video"
-      />
-      <label htmlFor="videoDescription" className="video-upload-form__label">
-        ADD A VIDEO DESCRIPTION
-      </label>
-      <textarea
-        name="videoDescription"
-        id="videoDescription"
-        className="video-upload-form__input video-upload-form__input--big"
-        placeholder="Add a description to your video"
-      ></textarea>
-      <button className="video-upload-form__button">PUBLISH</button>
-      <Link to="/" className="video-upload-form__cancel">
-        CANCEL
-      </Link>
+      <div className="video-upload-form__image-wrapper">
+        <p className="video-upload-form__label">VIDEO THUMBNAIL</p>
+        <img
+          src={videoThumbnail}
+          alt="sprinter at the starting line in blocks with a batton in hand"
+          className="video-upload-form__thumbnail"
+        />
+      </div>
+      <div className="video-upload-form__input-wrapper">
+        <label htmlFor="videoTitle" className="video-upload-form__label">
+          TITLE YOUR VIDEO
+        </label>
+        <input
+          type="text"
+          name="videoTitle"
+          id="videoTitle"
+          className="video-upload-form__input"
+          placeholder="Add a title to your video"
+        />
+        <label htmlFor="videoDescription" className="video-upload-form__label">
+          ADD A VIDEO DESCRIPTION
+        </label>
+        <textarea
+          name="videoDescription"
+          id="videoDescription"
+          className="video-upload-form__input video-upload-form__input--big"
+          placeholder="Add a description to your video"
+        ></textarea>
+      </div>
+      <div className="video-upload-form__button-wrapper">
+        <button className="video-upload-form__button">PUBLISH</button>
+        <Link to="/" className="video-upload-form__cancel">
+          CANCEL
+        </Link>
+      </div>
     </form>
   );
 }
